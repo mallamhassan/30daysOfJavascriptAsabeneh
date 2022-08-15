c384876c2fc8dc16f5dc6299c3102f4430015ad5
mallamhassan <hassanadeleke07@gmail.com>
2022-08-15 11:06:38 +0100

    feat:day1

---
+++ day3/script.js
@@ -0,0 +1,12 @@
+let base = prompt('enter base of a triangle');
+let height= prompt('enter height of triangle');
+let area = (0.5 * base * height);
+ alert(area);
+
+let a = prompt('enter side a');
+let b = prompt(`enter side b`);
+let c = prompt(`enter side c`);
+let perimeter = parseInt(a) + parseInt(b) + parseInt(c);
+alert(perimeter)
+
+let
